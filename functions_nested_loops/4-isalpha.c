ee923b6299d7baf0b1e5d608ef5b04060391b9c8
Nacho <6319@holbertonstudents.com>
2023-02-19 17:53:17 -0800

    idkrn

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -8,8 +8,8 @@
 
 int _isalpha(int c)
 {
-  if (((c > 96) && (c < 123)) || ((c > 64) && (c < 91)))
-    return (1);
-  else
-    return (0);
+if (((c > 96) && (c < 123)) || ((c > 64) && (c < 91)))
+return (1);
+else
+return (0);
 }
--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,10 +7,10 @@
 
 int print_last_digit(int n)
 {
-  int last = n % 10;
+int last = n % 10;
 
-  if (last < 0)
-    last *= -1;
-  _putchar(last + '0');
-  return (last);
+if (last < 0)
+last *= -1;
+_putchar(last + '0');
+return (last);
 }

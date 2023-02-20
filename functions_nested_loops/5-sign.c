9f93449b32814ca43e0442f1f38542539773fdc1
Nacho <6319@holbertonstudents.com>
2023-02-20 09:56:33 -0800

    Task 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,20 +1,30 @@
+#include <limits.h>
 #include "main.h"
 
-
-
-
+/**
+ * print_sign - function that prints the sign of a number.
+ * @n: input number.
+ * Return: 1 if ledssdsctter, 0 if else.
+ */
 
 int print_sign(int n);
 {
 if (n > 0)
+{
+_putchar ('+');
 return (1);
-printf("%d+");
+}
 
 if else(n < 0)
-return (-1)
-printf("%d-")
+{
+_putchar ('-');
+return (-1);
+}
 
 else
-return (0)
-printf("%d0")
+{
+return (0);
+_putchar ('0');
+}
+_putchar('\n');
 }

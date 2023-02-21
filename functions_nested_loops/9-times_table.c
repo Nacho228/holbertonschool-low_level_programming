77bf1555ff3fe44f8f535b7a2cf205fa8660a04d
Nacho <6319@holbertonstudents.com>
2023-02-21 10:14:19 -0800

    asd

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,49 +1,42 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * times_table - function that prints the 9 times table, starting with 0.
- * @n: int type number.
+ * times_table - Prints the 9 times table, starting with 0
+ *
+ * Return: empty output
  */
-
 void times_table(void)
 {
-int x = 0, y, z;
+int x, y, z, u, d;
 
-if (n > 15 || n < 0)
-return;
-while (x <= n)
+for (x = 0; x <= 9; x++)
 {
-for (y = 0; y <= n; y++)
+for (y = 0; y <= 9; y++)
 {
 z = x * y;
-if (z > 99)
+
+if (z > 9)
 {
-_putchar(z / 100 + '0');
-_putchar((z / 10 % 10) + '0');
-_putchar(z % 10 + '0');
-}
-else if (z > 9)
-{
-_putchar(' ');
-_putchar(z / 10 + '0');
-_putchar(z % 10 + '0');
-}
-else if (y != 0)
-{
-_putchar(' ');
-_putchar(' ');
-_putchar(z + '0');
+u = z % 10;
+d = (z - u) / 10;
+
+_putchar(44);
+_putchar(32);
+_putchar(d + '0');
+_putchar(u + '0');
 }
 else
-_putchar(z + '0');
-if (y != n)
 {
-_putchar(',');
-_putchar(' ');
+if (y != 0)
+{
+_putchar(44);
+_putchar(32);
+_putchar(32);
+}
+_putchar(z + '0');
 }
 }
+
 _putchar('\n');
-x++;
 }
 }

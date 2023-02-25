78b46b42949aa0e68a37b5fe9b9a3affa8b14eb9
Nacho <6319@holbertonstudents.com>
2023-02-24 18:47:48 -0800

    asd

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- * print_diagonal - function that draws a diagonal line in the terminal.
- * @n: Number of \ characters to print.
+ * print_triangle - function that draws a triangle line in the terminal.
+ * @size: Size of the triangle.
  */
 
-void print_diagonal(int n)
+void print_triangle(int size)
 {
 int c, d;
-if (n <= 0)
+if (size <= 0)
 {
 _putchar('\n');
 }
 else
 {
-for (c = 0 ; c < n; c++)
+for (c = 0 ; c < size; c++)
 {
 _putchar('\\');
 _putchar('\n');
-if (c + 1 < n)
+if (c + 1 < size)
 {
 for (d = c + 1; d > 0; d--)
 _putchar('#');

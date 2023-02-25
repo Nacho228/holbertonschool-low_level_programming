7f4d2dc45788883773ce03bacb5765801cf48970
Nacho <6319@holbertonstudents.com>
2023-02-24 17:46:38 -0800

    asd

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -5,20 +5,16 @@
 void more_numbers(void)
 {
 int i, n;
-{
 for (i = 0; i <= 9; i++)
 {
 for (n = 0; n <= 14; n++)
 {
-_putchar('\n');
 if (n / 10 != 0)
 {
 _putchar(n / 10 + '0');
 }
 _putchar(n % 10 + '0');
-{
 }
-}
-}
+_putchar('\n');
 }
 }

1960968cda08fbea05028c4089f16220d0404204
Nacho <6319@holbertonstudents.com>
2023-02-24 17:52:41 -0800

    asd

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,13 @@
 void print_most_numbers(void)
 {
 int c;
+
 for (c = 48 ; c <= 57 ; c++)
 {
-for (c != 50 ; c != 52;)
+if (c != 50 && c != 52)
 {
-_putchar('\n');
 _putchar(c);
+_putchar('\n');
 }
 }
 }

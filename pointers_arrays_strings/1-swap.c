ed71b425757746276c653864d3d6fda8e6bcff3f
Nacho <6319@holbertonstudents.com>
2023-02-28 06:51:49 -0800

    asd

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - pointer to an int as parameter and updates its value to 98.
+ * @a: int a.
+ * @b; int b.
+ * Return: void
+ */
+
+void swap_int (int *a, int *b)
+{
+int c;
+
+*a = c;
+*b = *a;
+c = *b;
+}

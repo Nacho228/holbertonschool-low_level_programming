14203871f6b9b3852dd8eaa47535bd2a629f6683
root <Nacho>
2023-03-02 09:18:20 -0800

    asd

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - a function that print a string in reverse
+ * @s: the char pointer
+ */
+void rev_string(char *s)
+{
+	int i = 0, counter = 0;
+	char walter_white;
+
+	while (s[i])
+	{
+		i++;
+	}	
+	for (; counter < i / 2; counter++)
+	{
+		walter_white = s[counter];
+		s[counter] = s[i - counter - 1];
+		s[i - counter - 1] = walter_white;
+	}
+}

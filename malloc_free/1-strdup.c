e644312b385498fc5d4c156b7533455d27001471
root <Nacho>
2023-03-17 14:32:34 -0700

    asd

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -11,6 +11,11 @@ char *_strdup(char *str)
 	int s, i = 0;
 	char *str2;
 
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
 	while (str[i] != '\0')
 	{
 		i++;
@@ -18,19 +23,20 @@ char *_strdup(char *str)
 
 	str2 = (char *) malloc((i + 1) * sizeof(char));
 
+	if (str2 == NULL)
+	{
+		return (NULL);
+	}
+
 	for (s = 0; s < i; s++)
 	{
 		str2[s] = str[s];
 	}
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-	if (_strdup(str2) < (i))
-	{
-		return(NULL);
-	}
+
+	str2[s] = '\0';
+
 	return (str2);
-free (str2);
+
+	free(str2);
 }
 

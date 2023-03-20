4c244f96173a143e2ca4b782b716d253e98ad935
root <Nacho>
2023-03-20 12:02:27 -0700

    asd

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef header
+#define header
+#include <stdio.h>
+/**
+ * struct dog - my dog name, age and owner.
+ * @name: her name.
+ * @age: her age.
+ * @owner: me.
+ *
+ * Description: "Define a new type struct dog with the following elements".
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif header

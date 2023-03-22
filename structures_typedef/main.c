fabbe8ab2b7a71de3f3f8ff85fd0570a604e3a12
root <Nacho>
2023-03-22 11:00:12 -0700

    task 2

--- structures_typedef/main.c
+++ structures_typedef/main.c
@@ -2,13 +2,17 @@
 #include "dog.h"
 
 /**
- *  * main - check the code .
+ *  * main - check the code
  *   *
  *    * Return: Always 0.
  *     */
 int main(void)
 {
-		init_dog(NULL, "Ghost", 4.75, "Jon Snow");
-			printf("Ok\n");
-				return (0);
+	    struct dog my_dog;
+
+	        my_dog.name = "Poppy";
+		    my_dog.age = 3.5;
+		        my_dog.owner = "Bob";
+			    print_dog(&my_dog);
+			        return (0);
 }

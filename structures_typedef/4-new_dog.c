36824fafca39d951e95a4f5c526d9984cd399113
root <Nacho>
2023-03-24 04:00:18 -0700

    asd

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -16,13 +16,28 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 	{
 		return (NULL);
-		free(new_dog);
 	}
 
 	new_dog->name = strdup(name);
+	
+	if(new_dog->name == NULL)
+	
+	{
+		free(new_dog);
+		return (NULL);
+	}
+
 	new_dog->age = age;
 	new_dog->owner = strdup(owner);
-
-	return (new_dog);
+	
+	if (new_dog->owner == NULL)
+	
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);	
+	}
+	
+	return(new_dog);
 }
 

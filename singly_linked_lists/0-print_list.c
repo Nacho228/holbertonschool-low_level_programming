1e0421940ab2639359c33901ac8666fd5fc23361
root <Nacho>
2023-04-05 06:05:26 -0700

    task 0 mostly done

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ *
+ */
+
+size_t print_list(const list_t *h)
+
+{
+  const list_t *node = h;
+  size_t count;
+  
+  while(node != NULL)
+  {
+  	if ((*node).str != NULL)
+  	{
+  		printf("%s\n", (*node).str);
+  	}
+	else
+	{
+		printf("[0] (nil)\n");
+	}
+	count++;
+	node  = (*node).next;
+  }
+
+return(count);  
+}

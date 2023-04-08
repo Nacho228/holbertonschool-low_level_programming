a99209274ac9ade0e3128e5d309c58e3c5269c23
root <Nacho>
2023-04-08 06:39:26 -0700

    task 0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ *
+ *
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *new = h;
+	size_t count = 0;
+
+	while (new != NULL)
+	{
+		printf("%u\n", new->n);
+		new = new->next;
+		count++;
+	}
+
+return (count);
+}
+

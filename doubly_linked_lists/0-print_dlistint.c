40f6711a468d0fd034d65f203f119f5ac9d50b95
root <Nacho>
2023-04-08 06:47:16 -0700

    case 2 of task 0 fixed

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 /**
- *
- *
+ * print_dlistint - prints all the elements of a dlistint_t list.
+ * @h: elements of the list.
+ * Return: number of the elements on the list.
  */
 size_t print_dlistint(const dlistint_t *h)
 {

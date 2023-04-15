5fd0a4e7b55d79cc50444c5f705af440b92d38e7
root <Nacho>
2023-04-15 12:07:16 -0700

    task 0 hash tables

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create -  function that creates a hash table.
+ * @size: size is the size of the array.
+ * Return: Returns a pointer to the newly created hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *new;
+
+	new = malloc(sizeof(hash_table_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->size = size;
+	if (size == 0)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->array = malloc(sizeof(hash_node_t) * size);
+	if (new->array == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		new->array[i] = NULL;
+	}
+return (new);
+}

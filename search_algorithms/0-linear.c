2ab56e8fc492e45ab8df7e7742ee93990125e070
Nacho228 <6319@holbertonstudents.com>
2023-05-25 16:07:28 -0700

    Linear seach algorithm done

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,1 +1,24 @@
-asd
+#include "search_algos.h"
+/**
+ * linear_search - search one by one items of the array.
+ * @array: array to look into.
+ * @size: size.
+ * @value: value to find.
+ * Return: index.
+*/
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (!array)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}

b15881f8908ab6fb25ee9ddb04c2d2c1aa0a883a
Nacho228 <6319@holbertonstudents.com>
2023-05-25 17:19:34 -0700

    asd

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -15,7 +15,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
-	printf("Value checked array [%d]\n", array[i]);
+	printf("Value checked array = [%d]\n", array[i]);
 		if (array[i] == value)
 		{
 			return (i);
